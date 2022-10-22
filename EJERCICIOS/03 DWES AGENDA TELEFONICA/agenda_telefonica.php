a70cc2819a20e9d17a0c42d6318f8616bdb0530c
Daniel Valcarce <dvalcarce93@gmail.com>
2022-10-22 17:05:31 +0200

    22OCT22

--- EJERCICIOS/03 DWES AGENDA TELEFONICA/agenda_telefonica.php
+++ EJERCICIOS/03 DWES AGENDA TELEFONICA/agenda_telefonica.php
@@ -1,35 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>AGENDA DE CONTACTOS TELEFONICOS</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
+    <h1>AGENDA DE CONTACTOS TELEFONICOS</h1>
+    <?php
+    //Comprobamos si nos pasan el array agenda o por el contrario esta vacío y tenemos que crearla
+    if(isset($_GET["agenda"])){
+        $agenda = $_GET["agenda"];
+    }else{
+        $agenda = array();
+    }
 
-error_reporting(1);
-//$nombreUsuario = $_GET["nombreUsuario"];
-//$numeroUsuario = $_GET["numeroUsuario"];
-$agenda = array();
+    //Comprobamos si el usuario nos envia el valor del submit
+    if(isset($_GET["submit"])){
+        $nombre_introducido = filter_input(INPUT_GET, "nombre_usuario");
+        $numero_introducido = filter_input(INPUT_GET, "numero_usuario");
+        //Programamos la reaccion a los diferentes campos vacíos o si todo está correcto
+        if(empty($nombre_introducido)){
+            echo ("<p> Debes introducir un nombre. </p>");
+        }elseif(empty($numero_introducido)){
+            unset($agenda[$nombre_introducido]);
+        }else{
+            $agenda[$nombre_introducido] = $numero_introducido;
+        }
+    }
+    ?>
+    <h2>Introducir contacto</h2>
+    <form>
+        <?php
+        foreach($agenda as $nombre => $numero){
+            echo "<input type=\"hidden\" name=\"agenda[" . $nombre . "]\" value=\" " . $numero . " \"/>";
+        }
+        ?>
+        <label for="nombre_usuario">Nombre : </label>
+        <input type="text" name="nombre_usuario"><br><br>
+        <label for="numero_usuario">Numero : </label>
+        <input type="number" name="numero_usuario"><br><br>
+        <input type="submit" name="submit"><br><br> <!--Se ha de introducir el name="submit" para que se procesen los valores -->
+    </form>
 
-$output = "<form action=\"\"><label for=\"nombreUsuario\">Nombre:</label><input type=\"text\" name=\"nombreUsuario\">
-<label for=\"numeroUsuario\">Número:</label><input type=\"text\" name=\"numeroUsuario\"><button type=\"submit\">Enviar
-</button>"; //formulario de inicio
+    <h2>Agenda</h2>
+    <?php //Mostramos todos los contactos de la agenda
 
-
-
-for($i=0; $i<count($_GET); $i++){
-    $agenda["Nombres"] = $_GET["nombreUsuario"];
-    $agenda["Numeros"] = $_GET["numeroUsuario"];    
-}
-
-foreach($agenda as $Nombres => $nombreUsuario){
-    $output = $output . "<input type=\"hidden\" name=\"nombresAgenda\" value=" . $agenda["Nombres"] . ">";
-}
-foreach($agenda as $Numeros => $numeroUsuario){
-    $output = $output . "<input type=\"hidden\" name=\"numerosAgenda\" value=" . $agenda["Numeros"] . "> <br>";
-}
-
-$output = $output . "</form>";
-
-var_dump($_GET);
-echo ($output);
-foreach($agenda as $Nombres => $nombreUsuario){
-    echo($agenda["Nombres"] . " : " . $agenda["Numeros"] . "<br>");
-}
-echo("<br>" .count($agenda))
-
-?>
+    echo "<ul>";
+    foreach($agenda as $nombre => $numero){
+        echo "<li>" . $nombre . " : " . $numero . "</li>";
+    }
+    echo "</ul>";
+    ?>
+</body>
+</html>
